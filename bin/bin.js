5fa4cfab7d9d44cf18c05e93f3389fe2f5264e8a
Weston Littrell <westonlittrell@gmail.com>
2017-08-16 08:44:47 -0500

    improve bin

--- bin/bin.js
+++ bin/bin.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 'use strict'
 
-const fs = require('fs');
+const fs = require('fs-extra');
 const main = require('../index.js');
 const path = require('path');
 const inquirer = require('inquirer');
 const ProgressBar = require('progress');
 const epubCheck = require('epub-check');
-//const handleOutput = require('handle-output');
+//const handleOutput = require('../lib/handle-output');
 
 process.on('unhandledRejection', (err) => {
   console.log(err);
@@ -28,10 +28,11 @@ if (path_provided) {
 
 const parseFile = (file_path) => {
   if (fs.existsSync(file_path) && fs.lstatSync(file_path).isFile() && file_path.endsWith('.xhtml')) {
+    const opts = setOpts(setPrompts());
     console.log(`Parsing Bible references in 1 file...`);
     const cwd = path.dirname(file_path);
     const file = path.basename(file_path);
-    const output = main(file_path);
+    const output = main(fs.readFileSync(file_path), opts);
     //fs.writeFileSync(path.resolve(cwd, file), output);
     //handleOutput(output);
   } else {
@@ -60,13 +61,28 @@ const initEpubParse = (cwd, validate) => {
   }
 }
 
-const parseEpubContent = (cwd) => {
+const parseEpubContent = (cwd, crossrc, opts) => {
   const text_dir = path.join(cwd, 'OEBPS/text');
+  const rc_loc = path.join(cwd, 'META-INF/crossrc.json');
+
+  const crossrc = (fs.existsSync(rc_loc)) ?
+    JSON.parse(fs.readFileSync(rc_loc, {encoding: 'utf8'})) : {};
+
+  const opts = setOpts(setPrompts(crossrc), crossrc);
+
+  if (crossrc !== {} && !crossrc.titleProps.versification) {
+    crossrc.titleProps.versification = vers;
+    fs.writeJson(rc_loc, crossrc, {spaces: 2});
+  }
 
   if (existsSync(text_dir)) {
     const files = fs.readdirSync(text_dir)
-      .filter(thing => fs.lstatSync(path.join(text_dir, thing)).isFile())
-      .filter(file => file.endsWith('.xhtml'));
+      .filter(thing => { return fs.lstatSync(path.join(text_dir, thing)).isFile() })
+      .filter(file => { return file.endsWith('.xhtml') })
+      .filter(file => { return !file.includes('index') })
+      .filter(file => { return !file.includes('bibliography') })
+      .filter(file => { return !file.includes('footnotes') })
+      .filter(file => { return !file.includes('copyright') && !file.includes('cover') && !file.includes('titlepage')});
 
     if (files.length === 0) throw new Error('No XHTML files found in the `OEBPS/text` directory.');
 
@@ -74,14 +90,107 @@ const parseEpubContent = (cwd) => {
 
     if (files.length > 1) console.log(`Parsing Bible references in ${files.length} files...`);
 
+    const bar = new ProgressBar('    Progress: [:bar] :rate/bps :percent', {
+      complete: '=',
+      incomplete: ' ',
+      width: 20,
+      total: files.length
+    });
+
     files.forEach(file => {
-      const output = main(path.resolve(cwd, file));
+      const output = main(path.resolve(cwd, file), opts);
       //fs.writeFileSync(path.resolve(cwd, file), output);
       //handleOutput(output);
+      bar.tick();
     });
   } else {
     throw new Error('`OEBPS/text` folder not found. Try again from an EPUB root directory.');
   }
+}
+
+const setPrompts = (crossrc = {}) => {
+  let prompts;
+  const trans_prompt = {
+    type: 'list',
+    name: 'translation',
+    message: 'What is the default translation?',
+    choices: [
+      'CSB/HCSB, ESV, AMP, NASB, etc.',
+      'CEB',
+      'KJV/NKJV or NIV',
+      'NAB or LXX',
+      'NLT or NCV',
+      'NRSV',
+      'Vulgate'
+    ],
+    default: 0
+  };
+
+  const lang_prompt = {
+    type: 'list',
+    name: 'language',
+    message: 'What is the primary language?',
+    choices: ['English', 'Spanish', 'Portuguese'],
+    default: 0
+  };
+
+  if (crossrc !== {}) {
+    prompts = (crossrc.titleProps.versification) ?
+      prompts = [] :
+      prompts = [trans_prompt];
+  } else {
+    prompts = [trans_prompt, lang_prompt];
+  }
+  return prompts;
+}
+
+const setOpts = (response, crossrc) => {
+  function getVersification(translation) {
+    switch (translation) {
+      case 'CSB/HCSB, ESV, AMP, NASB, etc.':
+        return 'default';
+      case 'CEB':
+        return 'ceb';
+      case 'KJV/NKJV or NIV':
+        return 'kjv';
+      case 'NAB or LXX':
+        return 'nab';
+      case 'NLT or NCV':
+        return 'nlt';
+      case 'NRSV':
+        return 'nrsv';
+      case 'Vulgate':
+        return 'vulgate';
+      default:
+        return 'default';
+    }
+  }
+
+  function getLang(language) {
+    switch (language) {
+      case 'English':
+        return 'en';
+      case 'Spanish':
+        return 'es';
+      case 'Portuguese':
+        return 'pt';
+      default:
+        return 'en';
+    }
+  }
+
+  inquirer.prompt(prompts)
+    .then(response => {
+      const vers = (response.translation) ?
+        getVersification(response.translation) :
+        crossrc.titleProps.versification;
+
+      const lang = (response.language) ?
+        getLang(response.language) :
+        crossrc.titleProps.language;
+
+      return {vers: vers, lang: lang};
+    });
 }
 
 console.log('\nAll done!');
--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 'use strict'
 
-/*const fs = require('fs');
-const path = require('path');*/
+const fs = require('fs-extra');
+const path = require('path');
+const { toJSON, toXHTML } = require('./lib/himalaya-io.js');
 
-const main = (param) => {
-  return param;
+const main = (path_or_html, opts = {vers: 'default', lang: 'en'}) => {
+  const json = (fs.existsSync(path_or_html)) ?
+    toJSON(fs.readFileSync(path_or_html, {encoding: 'utf8'})) :
+    toJSON(path_or_html);
+
+
 };
 
 module.exports = main;
