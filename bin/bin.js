1a532fb269274d450a1d41d5a18fdba56b32d673
Weston Littrell <westonlittrell@gmail.com>
2017-08-23 15:50:08 -0500

    more cleanup

--- bin/bin.js
+++ bin/bin.js
@@ -1,23 +1,21 @@
 #!/usr/bin/env node
-'use strict'
 
-const epubCheck = require('epub-check');
-const fs = require('fs-extra');
-const inquirer = require('inquirer');
-const main = require('../index.js');
-const path = require('path');
+const epubCheck = require('epub-check')
+const fs = require('fs-extra')
+const inquirer = require('inquirer')
+const main = require('../index.js')
+const path = require('path')
 
-//const handleOutput = require('../lib/handle-output');
 
 process.on('unhandledRejection', (err) => {
-  console.log(err);
-});
+  console.log(err)
+})
 
-const cwd = process.cwd();
-const skip_validate = process.argv.includes('-s');
+const cwd = process.cwd()
+const skip_validate = process.argv.includes('-s')
 
 const setPrompts = (crossrc = {}) => {
-  let prompts;
+  let prompts
   const trans_prompt = {
     type: 'list',
     name: 'translation',
@@ -32,7 +30,7 @@ const setPrompts = (crossrc = {}) => {
       'Vulgate'
     ],
     default: 0
-  };
+  }
 
   const lang_prompt = {
     type: 'list',
@@ -40,53 +38,53 @@ const setPrompts = (crossrc = {}) => {
     message: 'What is the primary language?',
     choices: ['English', 'Spanish', 'Portuguese'],
     default: 0
-  };
+  }
 
   if (crossrc.hasOwnProperty('titleProps')) {
-    prompts = (crossrc.titleProps.hasOwnProperty('versification')) ? [] : [trans_prompt];
+    prompts = crossrc.titleProps.hasOwnProperty('versification') ? [] : [trans_prompt]
   } else {
-    prompts = [trans_prompt, lang_prompt];
+    prompts = [trans_prompt, lang_prompt]
   }
-  return prompts;
-};
+  return prompts
+}
 
 const getVersification = (translation) => {
   switch (translation) {
-    case 'CSB/HCSB, ESV, AMP, NASB, etc.':
-      return 'default';
-    case 'CEB':
-      return 'ceb';
-    case 'KJV/NKJV or NIV':
-      return 'kjv';
-    case 'NAB or LXX':
-      return 'nab';
-    case 'NLT or NCV':
-      return 'nlt';
-    case 'NRSV':
-      return 'nrsv';
-    case 'Vulgate':
-      return 'vulgate';
-    default:
-      return 'default';
+  case 'CSB/HCSB, ESV, AMP, NASB, etc.':
+    return 'default'
+  case 'CEB':
+    return 'ceb'
+  case 'KJV/NKJV or NIV':
+    return 'kjv'
+  case 'NAB or LXX':
+    return 'nab'
+  case 'NLT or NCV':
+    return 'nlt'
+  case 'NRSV':
+    return 'nrsv'
+  case 'Vulgate':
+    return 'vulgate'
+  default:
+    return 'default'
   }
-};
+}
 
 const getLang = (language) => {
   switch (language) {
-    case 'English':
-      return 'en';
-    case 'Spanish':
-      return 'es';
-    case 'Portuguese':
-      return 'pt';
-    default:
-      return 'en';
+  case 'English':
+    return 'en'
+  case 'Spanish':
+    return 'es'
+  case 'Portuguese':
+    return 'pt'
+  default:
+    return 'en'
   }
-};
+}
 
-const parseEpubContent = (cwd) => {
-  const text_dir = path.join(cwd, 'OEBPS/text');
-  const rc_loc = path.join(cwd, 'META-INF/crossrc.json');
+const parseEpubContent = (dir) => {
+  const text_dir = path.join(dir, 'OEBPS/text')
+  const rc_loc = path.join(dir, 'META-INF/crossrc.json')
 
   if (fs.existsSync(text_dir)) {
     const files = fs.readdirSync(text_dir)
@@ -95,53 +93,53 @@ const parseEpubContent = (cwd) => {
       .filter(file => { return !file.includes('index') })
       .filter(file => { return !file.includes('bibliography') })
       .filter(file => { return !file.includes('footnotes') })
-      .filter(file => { return !file.includes('copyright') && !file.includes('cover') && !file.includes('titlepage')});
+      .filter(file => { return !file.includes('copyright') && !file.includes('cover') && !file.includes('titlepage')})
 
-    if (files.length === 0) throw new Error('No XHTML files found in the `OEBPS/text` directory.');
+    if (files.length === 0) throw new Error('No XHTML files found in the `OEBPS/text` directory.')
 
-    const crossrc = (fs.existsSync(rc_loc)) ? JSON.parse(fs.readFileSync(rc_loc, {encoding: 'utf8'})) : {};
+    const crossrc = fs.existsSync(rc_loc) ? JSON.parse(fs.readFileSync(rc_loc, {encoding: 'utf8'})) : {}
 
     inquirer.prompt(setPrompts(crossrc))
       .then(response => {
-        const vers = (response.translation) ?
+        const vers = response.translation ?
           getVersification(response.translation) :
-          crossrc.titleProps.versification;
+          crossrc.titleProps.versification
 
-        const lang = (response.language) ?
+        const lang = response.language ?
           getLang(response.language) :
-          crossrc.titleProps.language;
+          crossrc.titleProps.language
 
         if (crossrc.hasOwnProperty('titleProps') && !crossrc.titleProps.versification) {
-          crossrc.titleProps.versification = vers;
-          fs.writeJson(rc_loc, crossrc, {spaces: 2});
+          crossrc.titleProps.versification = vers
+          fs.writeJson(rc_loc, crossrc, {spaces: 2})
         }
 
-        if (files.length === 1) console.log('\nParsing Bible references in 1 file...\n');
-        if (files.length > 1) console.log(`\nParsing Bible references in ${files.length} files...\n`);
+        if (files.length === 1) console.log('\nParsing Bible references in 1 file...\n')
+        if (files.length > 1) console.log(`\nParsing Bible references in ${files.length} files...\n`)
 
-        main(text_dir, files, {vers: vers, lang: lang})
+        main(text_dir, files, { vers, lang })
           .then(() => { console.log('\nDone!') })
       })
   } else {
     throw new Error('`OEBPS/text` folder not found. Try again from an EPUB root directory.')
   }
-};
+}
 
 if (skip_validate) {
-  console.log(`👨🏼‍💻 skipping EpubCheck\n`);
-  parseEpubContent(cwd);
+  console.log(` 👨🏼‍💻 skipping EpubCheck\n`)
+  parseEpubContent(cwd)
 } else {
-  console.log(`Checking EPUB validity...`);
+  console.log(`Checking EPUB validity...`)
   epubCheck(cwd).then(data => {
     if (data.pass) {
-      console.log(`✔ Valid EPUB\n`);
-      parseEpubContent(cwd);
+      console.log(`✔ Valid EPUB\n`)
+      parseEpubContent(cwd)
     } else {
-      let err_msg = '✘ This EPUB is not valid. Fix errors and try again.\n';
+      let err_msg = '✘ This EPUB is not valid. Fix errors and try again.\n'
       data.messages.forEach(msg => {
-        err_msg += `\n${msg.type} | file: ${msg.file} line: ${msg.line} col: ${msg.col} | ${msg.msg}`;
-      });
-    throw err_msg;
+        err_msg += `\n${msg.type} | file: ${msg.file} line: ${msg.line} col: ${msg.col} | ${msg.msg}`
+      })
+      throw err_msg
     }
-  }).catch(err => {console.error(err);});
+  }).catch(err => { console.error(err) })
 }
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const deepCopyTagRefs = require('./lib/deep-copy-tag-refs')
 const fs = require('fs-extra')
 const { toJSON, toXHTML } = require('./lib/himalaya-io')
-const log = require('single-line-log').stdout;
+const log = require('single-line-log').stdout
 const path = require('path')
 const tagLocal = require('./lib/tag-local-orphans')
 
@@ -9,7 +9,7 @@ const main = (text_dir, files, opts = {vers: 'default', lang: 'en'}) => {
   // get explicit parse data and tagged content
   let all_data = files.map(file => {
     log(' - Tagging explicit refs: ' + file)
-    const json = toJSON(fs.readFileSync(path.join(text_dir, file), {encoding: 'utf8'}));
+    const json = toJSON(fs.readFileSync(path.join(text_dir, file), {encoding: 'utf8'}))
     const { tagged, data } = deepCopyTagRefs(json, 'explicit', opts, log, file)
     const html = toXHTML(tagged)
     log.clear()
@@ -50,7 +50,7 @@ const main = (text_dir, files, opts = {vers: 'default', lang: 'en'}) => {
   // tag remaining orphans using provided context
   all_data = all_data.map(file_data => {
     log(' - Tagging remaining orphans (using context tags): ' + file_data.name)
-    const json = toJSON(file_data.final_html);
+    const json = toJSON(file_data.final_html)
     const { tagged, data } = deepCopyTagRefs(json, 'context', opts, log, file_data.name)
     const html = toXHTML(tagged)
 
@@ -75,4 +75,4 @@ const main = (text_dir, files, opts = {vers: 'default', lang: 'en'}) => {
   return fs.outputJson(path.join(text_dir, `test/.percival/data-${new Date().toISOString()}.json`), all_data, {space: 2})
 }
 
-module.exports = main;
+module.exports = main
