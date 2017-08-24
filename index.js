15eceff841d6bfe14577556dbc9c9ac68634576b
Weston Littrell <westonlittrell@gmail.com>
2017-08-24 10:03:26 -0500

    refactor a bit

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ const main = (text_dir, files, opts = {vers: 'default', lang: 'en'}) => {
   // get explicit parse data and tagged content
   let all_data = files.map(file => {
     log(' - Tagging explicit refs: ' + file)
-    const json = toJSON(fs.readFileSync(path.join(text_dir, file), {encoding: 'utf8'}))
-    const { tagged, data } = deepCopyTagRefs(json, 'explicit', opts, log, file)
-    const html = toXHTML(tagged)
+    const { tagged, data } = deepCopyTagRefs(toJSON(fs.readFileSync(path.join(text_dir, file), {encoding: 'utf8'})), 'explicit', opts, log, file)
     log.clear()
 
     return {
@@ -20,49 +18,42 @@ const main = (text_dir, files, opts = {vers: 'default', lang: 'en'}) => {
       explicit: data,
       nearby: {},
       with_context: {},
-      final_html: html
+      final_html: toXHTML(tagged)
     }
   })
   log('')
   console.log(' ✔︎ Tagged explicit refs')
 
-  // get any missed local orphans
+  // locate and tag all orphans
   all_data = all_data.map(file_data => {
-    log(' - Tagging nearby orphans (nearby context): ' + file_data.name)
-    const { data, html } = tagLocal(file_data.final_html, opts)
+    log(' - Tagging orphaned refs: ' + file_data.name)
 
-    if (data.length > 0) {
-      file_data.nearby = data
-      file_data.final_html = html
+    const local = tagLocal(file_data.final_html, opts)
+
+    if (local.data.length > 0) {
+      file_data.nearby = local.data
+      file_data.final_html = local.html
     }
 
-    return file_data
-  })
-  log('')
-  console.log(' ✔︎ Tagged local orphans (nearby context)')
+    const remote = deepCopyTagRefs(toJSON(file_data.final_html), 'context', opts, log, file_data.name)
 
-  // tag remaining orphans using provided context
-  all_data = all_data.map(file_data => {
-    log(' - Tagging remaining orphans (using context tags): ' + file_data.name)
-    const json = toJSON(file_data.final_html)
-    const { tagged, data } = deepCopyTagRefs(json, 'context', opts, log, file_data.name)
-    const html = toXHTML(tagged)
-
-    if (data.length > 0) {
-      file_data.with_context = data
-      file_data.final_html = html
+    if (remote.data.length > 0) {
+      file_data.with_context = remote.data
+      file_data.final_html = toXHTML(remote.tagged)
     }
 
     log.clear()
     return file_data
   })
   log('')
-  console.log(' ✔︎ Tagged remaining orphans (given context)')
+  console.log(' ✔︎ Tagged orphaned refs')
 
+  // write html back to disk
   all_data.forEach(file_data => {
     fs.outputFileSync(path.join(text_dir, 'test', file_data.name), file_data.final_html)
   })
 
+  // write data to disk
   return fs.outputJson(path.join(text_dir, `test/.percival/data-${new Date().toISOString()}.json`), all_data, {space: 2})
 }
 
