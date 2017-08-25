c918a686b080eb12f1ae84a866edc218438b211d
Weston Littrell <westonlittrell@gmail.com>
2017-08-25 15:23:12 -0500

    99 problems but null aint one

--- test/assign-confidence.test.js
+++ test/assign-confidence.test.js
@@ -1,7 +1,5 @@
-'use strict'
-
 const test = require('ava');
-const assignConfidence = require('../lib/assign-confidence.js');
+const { assignConfidence } = require('../lib/assign-confidence.js');
 
 test('assign-confidence exists', t => {
   t.pass(true);
